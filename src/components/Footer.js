598fdf8871d9374cbed7bdc1c18545c2662e8dd9
Dorota Gil <gildota@wp.pl>
2020-10-07 23:30:36 +0200

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
+
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
+            <div className="copyright py-4 text-center text-white">
+              <div className="container">
+                <small>Copyright &copy; Dorota Gil</small>
+              </div>
+            </div>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
