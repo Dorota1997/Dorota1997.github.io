74059155ea9d7bf1430aba1e03af01d9fccb4915
Dorota Gil <gildota@wp.pl>
2020-10-07 23:29:23 +0200

    Update about component

--- src/components/About.js
+++ src/components/About.js
@@ -3,22 +3,26 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     if (this.props.data) {
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = 'images/' + this.props.data.image;
       var about = this.props.data.description;
     }
 
     return (
       <section id="about">
-         <div className="row">
-            <div className="twelve columns">
-               <div className="three columns">
-                  <img className="profile-picture"  src={profilepic} alt="Tim Baker Profile Pic"/>
-               </div>
-                  <div className="nine columns">
-                     <p>{about}</p>
-                  </div>
+        <div className="row">
+          <div className="twelve columns">
+            <div className="three columns">
+              <img
+                className="profile-picture"
+                src={profilepic}
+                alt="Dorota Gil Profile Pic"
+              />
             </div>
-         </div>
+            <div className="nine columns">
+              <p>{about}</p>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
