b93e497908ff5861182d8e5596c53af40cd0bee9
Dorota Gil <gildota@wp.pl>
2020-10-07 23:38:26 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
