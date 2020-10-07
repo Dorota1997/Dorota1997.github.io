85fb94903c2c4f30b4f448a0ec730ccc89400829
Dorota Gil <gildota@wp.pl>
2020-10-07 23:27:21 +0200

    Render skills data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class Skills extends Component {
+  render() {
+    if (this.props.data) {
+      var skills = this.props.data.skills.map(function (skills, i) {
+        return (
+          <i className={skills.class} key={i}>
+            <p className="text-center">{skills.name}</p>
+          </i>
+        );
+      });
+    }
+
+    return (
+      <section id="testimonials">
+        <div className="text-container">
+          <div className="row">
+            <div className="row text-white">
+              <div className="twelve columns">
+                <h1>
+                  <span className="text-white">Umiejętności</span>
+                </h1>
+              </div>
+            </div>
+            <div className="twelve columns skill-icon">{skills}</div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Skills;
