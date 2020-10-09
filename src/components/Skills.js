bd3536a2fe54507c62469af3a262a5f235341f50
Dorota Gil <gildota@wp.pl>
2020-10-09 22:30:11 +0200

    Modify skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 class Skills extends Component {
   render() {
@@ -6,14 +7,14 @@ class Skills extends Component {
       var skills = this.props.data.skills.map(function (skills, i) {
         return (
           <div className="col-md-12" key={i}>
-            <div className="row mt-4" >
+            <div className="row mt-4">
               <div className="col-md-3">
-                <i className={skills.class}><p className="mr-3 ml-3">{skills.name}</p></i>
+                <i className={skills.class}>
+                  <p className="mr-3 ml-3">{skills.name}</p>
+                </i>
               </div>
               <div className="col-md-9">
-                <div className="progress progress-value">
-                  <div className="progress-bar bg-warning w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                <ProgressBar className="progress-value" now={skills.level} />
               </div>
             </div>
           </div>
@@ -22,14 +23,14 @@ class Skills extends Component {
     }
 
     return (
-      <section id="skills" >
+      <section id="skills">
         <div className="col-md-12">
           <div className="col-md-12">
             <h1 className="section-title">
               <span className="text-white">Umiejętności</span>
             </h1>
           </div>
-          <div className="col-md-12 mx-auto"> 
+          <div className="col-md-12 mx-auto">
             <div className="col-md-10 mx-auto">
               <div className="skill-icon">{skills}</div>
             </div>
