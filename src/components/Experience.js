b3cb93d3c06ff317083fd87b7103aff1c4c1fea2
Dorota Gil <gildota@wp.pl>
2020-10-11 23:50:00 +0200

    Get experience data and generate experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
 class Experience extends Component {
+  render() {
+    if (this.props.data) {
+      var work = this.props.data.work.map(function (work, i) {
+        return (
+          <div key={i} className="pb-5 col-md-12">
+            <p className="info">
+              {work.years}
+              <br />
+              {work.company}
+              <br />
+              {work.title}
+            </p>
+            <p className="col-md-6 experience-desc">{work.description}</p>
+          </div>
+        );
+      });
+    }
+
+    return (
+      <section id="resume">
+        <div className="col-md-12 mx-auto">
+          <div className="col-md-12">
+            <h1 className="section-title" style={{ color: 'black' }}>
+              <span className="text-black">Doświadczenie</span>
+            </h1>
+          </div>
+          <div className="col-md-12 work main-col">{work}</div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Experience;
