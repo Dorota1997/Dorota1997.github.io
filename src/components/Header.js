b1095fcc24308e9ae30fbe7164a0412454d1e588
Dorota Gil <gildota@wp.pl>
2020-10-13 23:38:50 +0200

    Set moving letters in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
+import { GreatThings, BeautifulQuestions, SignalNoise, Thursday, ReadySetGo, SunnyMorning } from 'moving-letters'
 
 class Header extends Component {
   render() {
     if (this.props.data) {
       var name = this.props.data.name;
       var title = this.props.data.title;
-      var networks = this.props.data.social.map(function (network) {
-        return (
-          <li key={network.name}>
-            <a href={network.url}>
-              <i className={network.className}></i>
-            </a>
-          </li>
-        );
-      });
     }
 
     return (
       <header id="home">
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
-        </nav>
-
         <div className="row banner">
           <div className="banner-text">
-            <h3>Cześć, jestem {name}</h3>
+            <GreatThings text={'Cześć jestem ' + name} />
             <h5 className="mx-auto">{title}</h5>
           </div>
         </div>
