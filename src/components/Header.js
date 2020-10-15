fea84233f1137289e056ef9d70b469d06e395626
Dorota Gil <gildota@wp.pl>
2020-10-15 23:55:17 +0200

    Remove unnecessary imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { GreatThings, BeautifulQuestions, SignalNoise, Thursday, ReadySetGo, SunnyMorning } from 'moving-letters'
+import { GreatThings } from 'moving-letters'
 
 class Header extends Component {
   render() {
