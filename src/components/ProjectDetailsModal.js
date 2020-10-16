c9d2e62f597f3670f7251625824b176c192ec848
Dorota Gil <gildota@wp.pl>
2020-10-17 00:21:35 +0200

    Display title and description with technologies in modal

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -7,6 +7,30 @@ class ProjectDetailsModal extends Component {
   }
 
   render() {
+    if (this.props.data) {
+      const technologies = this.props.data.technologies;
+      var title = this.props.data.title;
+      var description = this.props.data.description;
+      if (this.props.data.technologies) {
+        {
+          var tech = technologies.map((icons, i) => {
+            return (
+              <li className="list-inline-item mx-3" key={i}>
+                <a>
+                  <div className="text-center">
+                    <i className={icons.class} style={{ fontSize: '300%' }}>
+                      <p className="text-center" style={{ fontSize: '30%' }}>
+                        {icons.name}
+                      </p>
+                    </i>
+                  </div>
+                </a>
+              </li>
+            );
+          });
+        }
+      }
+    }
     return (
       <Modal
         {...this.props}
@@ -17,6 +41,18 @@ class ProjectDetailsModal extends Component {
         <a onClick={this.props.onHide} className="modal-close">
           <i className="fas fa-times fa-3x close-icon"></i>
         </a>
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-md-7"></div>
+            <div className="col-md-5">
+              <h3>{title}</h3>
+              <p className="modal-description">{description}</p>
+              <div className="col-md-12 text-center">
+                <ul className="list-inline mx-auto">{tech}</ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </Modal>
     );
   }
