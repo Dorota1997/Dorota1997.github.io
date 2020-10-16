5315f3028d54486ca45ed8dce84939f3c1c9b324
Dorota Gil <gildota@wp.pl>
2020-10-17 00:20:30 +0200

    Display modal with close button

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 class ProjectDetailsModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <a onClick={this.props.onHide} className="modal-close">
+          <i className="fas fa-times fa-3x close-icon"></i>
+        </a>
+      </Modal>
+    );
+  }
 }
 
 export default ProjectDetailsModal;
