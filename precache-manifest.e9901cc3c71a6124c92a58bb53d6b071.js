6f4c0ee03c33b1945e4d5350272d359170f8cabb
Dorota Gil <gildota@wp.pl>
2020-11-10 20:33:36 +0100

    Updates

---
+++ precache-manifest.e9901cc3c71a6124c92a58bb53d6b071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955945a109eafb9dc9cf86469d9c7ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a2ffcd4cd2bcbe6ad46",
+    "url": "/static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "88856f6360c5422d4638",
+    "url": "/static/css/main.ad7273c8.chunk.css"
+  },
+  {
+    "revision": "1a2ffcd4cd2bcbe6ad46",
+    "url": "/static/js/2.f152b173.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.f152b173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88856f6360c5422d4638",
+    "url": "/static/js/main.1aa9d570.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
