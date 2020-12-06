5bf5dfb0745e7ecaf46021e5cb13ebebd5228f11
Dorota Gil <gildota@wp.pl>
2020-12-06 19:37:35 +0100

    Declare global variables

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
